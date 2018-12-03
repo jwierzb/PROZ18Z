e7065665ff6f83f84a61533c7973554d4d75765b
jwierzb <44870703+jwierzb@users.noreply.github.com>
2018-12-03 01:14:57 +0100

    Created exception's directory. Added exception class and handler for password authentication.

---
+++ src/main/java/com/proz2018/exception/InvalidPasswordException.java
@@ -0,0 +1,9 @@
+package com.proz2018.exception;
+
+
+public class InvalidPasswordException extends RuntimeException {
+
+    public InvalidPasswordException(String username) {
+        super("Invalid password for "+ username);
+    }
+}
