1afa4d042ca55e7b4445778794bd25a92949f51f
jwierzb <44870703+jwierzb@users.noreply.github.com>
2018-12-03 01:15:48 +0100

    Modified user entity with respect to authorization system.

---
+++ src/main/java/com/proz2018/entities/User.java
@@ -0,0 +1,87 @@
+package com.proz2018.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Email;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Collection;
+
+@Entity
+@Data
+@Table(name = "user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+
+    @Column(name = "user_password")
+    @JsonIgnore
+    private String userPassword;
+
+
+    @Column(name = "last_login")
+    @CreationTimestamp
+    private Timestamp lastLogin;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+
+    @Column(name = "email")
+    @Email
+    private String email;
+
+    @Override
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public String getPassword() {
+        return userPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+
+}
