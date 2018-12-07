73f446801a798d3bc712bd22a91feb9398127bc6
jwierzb <wierzbicki.jakub@yahoo.com>
2018-12-07 05:29:10 +0100

    Devices pagable searching method added

--- src/main/java/com/proz2018/dao/DeviceDao.java
+++ src/main/java/com/proz2018/dao/DeviceDao.java
@@ -2,10 +2,13 @@ package com.proz2018.dao;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.proz2018.entities.Device;
+import com.proz2018.entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,5 +18,6 @@ public interface DeviceDao extends JpaRepository<Device, Integer> {
     List<Device> findByUserId(Integer id);
     Optional<Device> findByUserIdAndId(Integer userId, Integer id);
     List<Device> findAllByUserId(Integer userId);
-    List<Device> findAllByUserIdOrderByDeviceNameAsc(Integer userId, Pageable page);
+    Page<Device> findAllByUser(User user, Pageable pageable);
+
 }
